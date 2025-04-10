cc3999b0d991c1df16bc906649bb5f2893db3dc4
Reche Soares <rechesoares13@gmail.com>
2025-04-10 13:00:01 +0530

    status color update

--- src/components/response/ResponseHeader.tsx
+++ src/components/response/ResponseHeader.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/cn";
 import React from "react";
 
 interface Props {
@@ -8,13 +9,22 @@ interface Props {
 }
 
 const ResponseHeader = ({ status, statusText, timeTaken, size }: Props) => {
+  const getStatusColor = (status: number): string => {
+    if (status >= 100 && status < 200) return "text-blue-500";
+    if (status >= 200 && status < 300) return "text-green-500";
+    if (status >= 300 && status < 400) return "text-indigo-500";
+    if (status >= 400 && status < 500) return "text-red-500";
+    if (status >= 500 && status < 600) return "text-red-500";
+    return "text-gray-500";
+  };
+
   return (
     <div className="w-full p-4 bg-bg-light-pri dark:bg-bg-dark-pri flex items-center gap-6 border-b border-stroke-light-ter dark:border-stroke-dark-ter">
       <div className="flex items-center gap-2">
         <span className="text-xs font-medium text-text-b-sec dark:text-text-w-sec">
           Status
         </span>
-        <span className="text-xs font-semibold text-green-500">
+        <span className={cn("text-xs font-semibold", getStatusColor(status))}>
           {status} {statusText}
         </span>
       </div>
@@ -22,7 +32,7 @@ const ResponseHeader = ({ status, statusText, timeTaken, size }: Props) => {
         <span className="text-xs font-medium text-text-b-sec dark:text-text-w-sec">
           Time
         </span>
-        <span className="text-xs font-semibold text-green-500">
+        <span className={cn("text-xs font-semibold", getStatusColor(status))}>
           {timeTaken.toFixed(1)} ms
         </span>
       </div>
@@ -30,7 +40,9 @@ const ResponseHeader = ({ status, statusText, timeTaken, size }: Props) => {
         <span className="text-xs font-medium text-text-b-sec dark:text-text-w-sec">
           Size
         </span>
-        <span className="text-xs font-semibold text-green-500">{size} b</span>
+        <span className={cn("text-xs font-semibold", getStatusColor(status))}>
+          {size} B
+        </span>
       </div>
     </div>
   );
